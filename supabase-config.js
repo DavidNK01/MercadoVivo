f3e7a420a418a1ab6c031f8a8a2d6ede7aebaf06
DavidNK01 <jesdavidrodriguez@gmail.com>
2026-08-28 09:08:03 -0400

    entrar los datos para aceder a base de datos

--- supabase-config.js
+++ supabase-config.js
@@ -6,8 +6,8 @@
 // 3. Copia "Project URL" y "anon public key" aquí abajo
 // ============================================================
 
-const SUPABASE_URL = "https://TU-PROYECTO.supabase.co";
-const SUPABASE_ANON_KEY = "TU-CLAVE-ANON-PUBLICA";
+const SUPABASE_URL = "https://ujpgsamhdiutpobsttbr.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "sb_publishable_zZWj1gWvTCBUmxKr2odusg_aymMYInl";
 
 // Cliente global reutilizado por todas las páginas.
 // Requiere que el <script> de supabase-js se cargue ANTES que este archivo.
